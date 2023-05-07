5da4bb90a68d569ece63be8d9b7f83686cc9fd67
Tony-Kovalov <tony.kovalov@neatapps.co>
2023-05-07 20:49:39 +0300

    Implement bottom navigation bar

--- lib/features/home_menu/presentation/pages/home.dart
+++ lib/features/home_menu/presentation/pages/home.dart
@@ -2,28 +2,26 @@ import 'package:camps_program/app_colors.dart';
 import 'package:camps_program/features/home_menu/presentation/pages/program_menu.dart';
 import 'package:camps_program/features/home_menu/presentation/widgets/card.dart';
 import 'package:camps_program/features/home_menu/presentation/widgets/yellow_long_card.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: const [
-            Text("5/4", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36)),
-            SizedBox(width: 10),
-            Text("МІСЦЕ\nДЕ ТРЕНУЮТЬ\nПЕРЕМОЖЦІВ", style: TextStyle(fontSize: 10)),
-          ],
-        ),
+        title: Image.asset("images/logo.png",),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.only(left: 16, top: 16, right: 16),
         child: SingleChildScrollView(
           child: Column(
             children: [
@@ -82,6 +80,109 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: Column(
+              children: const [
+                Icon(Icons.settings_outlined, color: Colors.black, size: 35),
+                SizedBox(height: 3),
+                Text("Налаштування", style: TextStyle(fontSize: 9)),
+              ],
+            ),
+            activeIcon: Column(
+              children: [
+                Icon(Icons.settings_outlined, color: Colors.purple[100], size: 35),
+                const SizedBox(height: 3),
+                const Text("Налаштування", style: TextStyle(fontSize: 9, color: Color(0xFFD8B7DF))),
+              ],
+            ),
+            label: 'Calls',
+          ),
+          BottomNavigationBarItem(
+            icon: Column(
+              children: const [
+                Icon(Icons.list_alt, color: Colors.black, size: 35),
+                SizedBox(height: 3),
+                Text("Завдання", style: TextStyle(fontSize: 9)),
+              ],
+            ),
+            activeIcon: Column(
+              children: [
+                Icon(Icons.list_alt, color: Colors.purple[100], size: 35),
+                const SizedBox(height: 3),
+                const Text("Завдання", style: TextStyle(fontSize: 9, color: Color(0xFFD8B7DF))),
+              ],
+            ),
+            label: 'Calls',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.primaryYellow,
+                border: Border.all(color: Colors.black),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(10),
+                child: Icon(
+                  Icons.home_outlined,
+                  color: Colors.black,
+                  size: 35,
+                ),
+              ),
+            ),
+            label: "Головна",
+          ),
+          BottomNavigationBarItem(
+            icon: Column(
+              children: const [
+                Icon(Icons.note_alt_outlined, color: Colors.black, size: 35),
+                SizedBox(height: 3),
+                Text("Нотатки", style: TextStyle(fontSize: 9)),
+              ],
+            ),
+            activeIcon: Column(
+              children: [
+                Icon(Icons.note_alt_outlined, color: Colors.purple[100], size: 35),
+                const SizedBox(height: 3),
+                const Text("Нотатки", style: TextStyle(fontSize: 9, color: Color(0xFFD8B7DF))),
+              ],
+            ),
+            label: 'Calls',
+          ),
+          BottomNavigationBarItem(
+            icon: Column(
+              children: const [
+                Icon(Icons.access_time_outlined, color: Colors.black, size: 35),
+                SizedBox(height: 3),
+                Text("Розклад", style: TextStyle(fontSize: 9)),
+              ],
+            ),
+            activeIcon: Column(
+              children: [
+                Icon(Icons.access_time_outlined, color: Colors.purple[100], size: 35),
+                const SizedBox(height: 3),
+                const Text("Розклад", style: TextStyle(fontSize: 9, color: Color(0xFFD8B7DF))),
+              ],
+            ),
+            label: 'Calls',
+          ),
+        ],
+      ),
     );
+  }
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
